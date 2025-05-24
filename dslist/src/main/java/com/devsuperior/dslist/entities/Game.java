c7a27014b70ca490f5c2f67eea3cc63142ff8e79
marcuscgc-code <marcuscgc@gmail.com>
2025-05-24 17:11:55 -0300

    seed dos games

--- dslist/src/main/java/com/devsuperior/dslist/entities/Game.java
+++ dslist/src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -18,17 +18,26 @@ public class Game {
     @Column(name="game_year")
     private Integer year;
     private String genre;
-    private String platform;
+    private String platforms;
+    private Double score;
     private String imgUrl;
+    @Column(columnDefinition= "TEXT")
     private String shortDescription;
+    // instrucao pra JPA na hora de gerar o banco, gera espaco pra TEXTO e nao pra varchar
+    @Column(columnDefinition= "TEXT")
     private String longDescription;
-    public Game(Long id, String title, Integer year, String genre, String platform, String imgUrl,
+
+    public Game(){
+
+    }
+    public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
             String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
-        this.platform = platform;
+        this.platforms = platforms;
+        this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
         this.longDescription = longDescription;
@@ -57,11 +66,11 @@ public class Game {
     public void setGenre(String genre) {
         this.genre = genre;
     }
-    public String getPlatform() {
-        return platform;
+    public String getPlatforms() {
+        return platforms;
     }
-    public void setPlatform(String platform) {
-        this.platform = platform;
+    public void setPlatforms(String platforms) {
+        this.platforms = platforms;
     }
     public String getImgUrl() {
         return imgUrl;
@@ -80,6 +89,12 @@ public class Game {
     }
     public void setLongDescription(String longDescription) {
         this.longDescription = longDescription;
+    }
+    public Double getScore() {
+        return score;
+    }
+    public void setScore(Double score) {
+        this.score = score;
     }
     
     
